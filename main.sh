f04b7833cf98e31e7232ec65f06aad3520db307d
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2025-04-02 12:21:57 +0200

    initial idea

---
+++ main.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+# https://big.oisd.nl/
+# https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+# https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
+
