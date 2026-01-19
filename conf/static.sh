8e0e93e97da2d51d2c03319a798634691ba876d9
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2026-01-19 11:19:16 +0100

    Create static.sh

---
+++ conf/static.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+POM=$(wget -O - https://raw.githubusercontent.com/Turtlecute33/toolz/refs/heads/master/src/data/adblock_data.json )
+
+POM=$( echo $POM | jq 'del( ."Error Trackers"."Sentry", ."OEMs"."Apple" )' )
+
+POM=$( echo $POM | jq -r '.. | arrays? | .[]' )
+
+echo $POM | sed 's/^/local-zone: "/' | sed 's/$/" always_nxdomain/' > 
