f511af73b4028ca9ae9eb5263132549654bd23c7
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2026-01-27 11:47:30 +0100

    Update notracking.sh

--- conf/notracking.sh
+++ conf/notracking.sh
@@ -6,4 +6,4 @@ LIST=$( echo "$LIST" | grep '0.0.0.0' | sed 's/0\.0\.0\.0\ //' | sort )
 
 echo " > New list contains "$(echo "$LIST" | wc -l)" domains"
 
-echo "$LIST" | sed 's/^/local-zone: "/' | sed 's/$/" always_nxdomain/' > ../tmp/notracking.txt
+echo "$LIST" | sed 's/^/local-zone: "/' | sed 's/$/" always_nxdomain/' > ../tmp/notracking.conf
