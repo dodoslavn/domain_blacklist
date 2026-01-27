29f9dad83d9c83fb453207de0cd5775b6ddcd1ed
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2026-01-27 14:05:35 +0100

    Update static.sh

--- conf/static.sh
+++ conf/static.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-
-POM=$(wget -O - https://raw.githubusercontent.com/Turtlecute33/toolz/refs/heads/master/src/data/adblock_data.json )
+POM=$(wget -O - https://raw.githubusercontent.com/Turtlecute33/toolz/refs/heads/master/src/data/adblock_data.json 2>/dev/null )
 
 POM=$( echo $POM | jq 'del( ."Error Trackers"."Sentry", ."OEMs"."Apple" )' )
 
 POM=$( echo $POM | jq -r '.. | arrays? | .[]' )
 
-echo $POM | sed 's/^/local-zone: "/' | sed 's/$/" always_nxdomain/' > /etc/unbound/unbound.conf.d/custom_blacklist.conf.d/static.conf
+echo "$POM" | sed 's/^/local-zone: "/' | sed 's/$/" always_nxdomain/' > /etc/unbound/unbound.conf.d/custom_blacklist.conf.d/static.conf
+
+echo "############"
+echo "### static"
+echo " > New static list contains $( cat /etc/unbound/unbound.conf.d/custom_blacklist.conf.d/static.conf | wc -l ) domains"
