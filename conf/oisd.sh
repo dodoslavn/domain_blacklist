22098055ad4659e4377bf9b8484dc7a1d4a2a3ad
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2026-01-27 11:47:21 +0100

    Change output file from oisd.txt to oisd.conf

--- conf/oisd.sh
+++ conf/oisd.sh
@@ -6,4 +6,4 @@ LIST=$(echo "$LIST" | grep ^'||' | sort )
 
 echo " > New list contains "$( echo "$LIST" | wc -l )" domains"
 
-echo "$LIST" | sed 's/|//g' | sed 's/\^//' | sed 's/^/local-zone: "/' | sed 's/$/" always_nxdomain/' > ../tmp/oisd.txt
+echo "$LIST" | sed 's/|//g' | sed 's/\^//' | sed 's/^/local-zone: "/' | sed 's/$/" always_nxdomain/' > ../tmp/oisd.conf
