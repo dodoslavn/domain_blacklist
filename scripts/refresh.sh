a068d19482f3ea83f2ed4ea94046deafff2cee04
dodoslavn <34769717+dodoslavn@users.noreply.github.com>
2026-09-04 11:06:39 +0200

    Update refresh.sh

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -10,6 +10,21 @@ cd "$(dirname "$0")"
 
 . ../conf/config.sh
 
+if [ "$(systemctl is-active unbound.service)" = "inactive" ] 
+	then
+	echo "WARNING: Skipping refresh, unbound is not running!" 
+	exit 0
+	fi
+
+unbound-checkconf 1>/dev/null 2>/dev/null
+RC=$?
+
+if [ "$RC" -ne 0 ]
+	then
+	echo "ERROR: Found issues in the unbound config!"
+	exit 0
+	fi
+
 mkdir -p ../tmp/ 
 rm -f ../tmp/*.conf
 
@@ -20,6 +35,20 @@ for S in $LIST;
 	../conf/"$S".sh
 	done
 
+
 cp ../tmp/*.conf /etc/unbound/unbound.conf.d/custom_blacklist.conf.d/
+echo '# Do NOT edit - config file managed by blacklisting service
+server:
+        include: /etc/unbound/unbound.conf.d/custom_blacklist.conf.d/*.conf' > /etc/unbound/unbound.conf.d/custom_blacklist.conf
+
+unbound-checkconf 1>/dev/null 2>/dev/null
+RC=$?
+if [ "$RC" -ne 0 ]
+        then
+        echo "ERROR: Found issues in Unbound configuration files! Probably caused by custom domain blacklisting service, reverting.."
+	echo "# custom blacklisting disabled automatically, new configuration didnt work" > /etc/unbound/unbound.conf.d/custom_blacklist.conf	
+        exit 0
+        fi
 
 systemctl reload unbound
+echo 'INFO: New configuration applied'
